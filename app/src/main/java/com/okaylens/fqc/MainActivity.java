bc0b62c984b6e19f89545035eea7afc1d85d1834
charleszhang1206 <15152239098@163.COM>
2017-05-04 08:44:07 +0800

    add grade

--- app/src/main/java/com/okaylens/fqc/MainActivity.java
+++ app/src/main/java/com/okaylens/fqc/MainActivity.java
@@ -1,13 +1,27 @@
 package com.okaylens.fqc;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_auto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_auto = (Button) findViewById(R.id.button10);
+        btn_auto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,SensorsAct.class);
+                startActivity(intent);
+            }
+        });
     }
 }
