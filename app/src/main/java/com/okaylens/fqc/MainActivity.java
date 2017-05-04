1476dee18c833acb4d9bb881e449595c5350e082
Tom <tom-li@okaylens.com>
2017-05-04 09:53:35 +0800

    debug main

--- app/src/main/java/com/okaylens/fqc/MainActivity.java
+++ app/src/main/java/com/okaylens/fqc/MainActivity.java
@@ -1,10 +1,7 @@
 package com.okaylens.fqc;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_auto = (Button) findViewById(R.id.button10);
-        btn_auto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,SensorsAct.class);
-                startActivity(intent);
-            }
-        });
     }
 }
